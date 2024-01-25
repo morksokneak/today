0fae29197d4d6c8e885a30ee30e391b14c479301
morksokneak <morksokneak@gmail.com>
2024-01-25 19:57:08 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello sokneak");
+        System.out.println("can u do it? ");
+        System.out.println("i can't do it.");
+    }
+}
